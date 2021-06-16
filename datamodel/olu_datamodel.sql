14ebcf3969403141d4d6bdf6846b4f3978b87a3d
Michal Kepka <mkepka@zcu.cz>
2021-06-17 01:54:01 +0200

    Adding initial SQL script

---
+++ datamodel/olu_datamodel.sql
@@ -0,0 +1,4 @@
+/*MIT License
+
+Copyright 2020-2021 Plan4all z.s.; SIA Baltic Open Solutions Center; Lesprojekt-služby, s.r.o.; WIRELESSINFO; Masarykova univerzita; Západočeská univerzita v Plzni
+*/
